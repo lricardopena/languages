a803d6fc8dfe5b96b010d5f1e26568d428c179ac
Luis Pena <luis.pena@trackstreet.com>
2020-10-25 01:25:17 -0600

    Add inheritance from the LStart class to reuse code

--- lstar_regular_languages/lstart_from_logs.py
+++ lstar_regular_languages/lstart_from_logs.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from graphviz import Digraph
+
+from lstar_regular_languages.lstart import LStartRegularLanguages
 
 EPSILON = '\u03B5'
 UPPER = "UPPER"
@@ -9,7 +10,7 @@ TYPE_TABLE = "type_table"
 STATE = "state"
 
 
-class LStartRegularLanguagesFromLog:
+class LStartRegularLanguagesFromLog(LStartRegularLanguages):
     table: pd.DataFrame
 
     def __init__(self, path_log: str):
@@ -21,12 +22,7 @@ class LStartRegularLanguagesFromLog:
 
         self.alphabet = self.get_alphabet_from_language()
 
-        self.running = True
-
-        self.table = pd.DataFrame(columns=[TYPE_TABLE, STATE, EPSILON]).set_index([TYPE_TABLE, STATE])
-
-        self.cache_accepted_strings = {}
-
+        super().__init__(set(self.alphabet), initialize=False)
         self.init_table()
 
     def get_alphabet_from_language(self) -> list:
@@ -39,8 +35,6 @@ class LStartRegularLanguagesFromLog:
         return sorted(list(alphabet))
 
     def ask_if_string_belongs_language(self, str_to_ask):
-        if str_to_ask == '0 1 1 0 0':
-            print('')
         if str_to_ask in self.cache_accepted_strings:
             return self.cache_accepted_strings[str_to_ask]
 
@@ -56,32 +50,6 @@ class LStartRegularLanguagesFromLog:
             return str1
 
         return f"{str1} {str2}"
-
-    def add_rows_to_table(self, data: dict):
-        index_element_delete = []
-        for i, state in enumerate(data[STATE]):
-            if state in self.table.index.get_level_values(1).values:
-                index_element_delete.append(i)
-
-        for i in sorted(index_element_delete, reverse=True):
-            for k in data.keys():
-                element_remove = data[k][i]
-                data[k].remove(element_remove)
-
-        if len(data[STATE]) > 0:
-            df = pd.DataFrame.from_dict(data, dtype=str)
-
-            for c in df.columns:
-                df[c].astype(str)
-
-            df.set_index([TYPE_TABLE, STATE], inplace=True)
-
-            self.table = pd.concat([self.table, df], sort=True)
-
-            self.table.sort_index(inplace=True)
-
-            for c in self.table.columns:
-                self.table[c].astype(str)
 
     def init_table(self):
         dict_to_add = {STATE: [], EPSILON: [], TYPE_TABLE: []}
@@ -101,125 +69,6 @@ class LStartRegularLanguagesFromLog:
             dict_to_add[EPSILON].append(sigma_belongs_language)
 
         self.add_rows_to_table(dict_to_add)
-
-    def add_column(self, sigma):
-        if sigma in self.table:
-            return False
-
-        experiment_values = []
-
-        for state_value in self.table.index.get_level_values(1).values:
-            str_to_ask = self.concatenate_two_strings(state_value, sigma)
-            value_to_add = self.ask_if_string_belongs_language(str_to_ask)
-            experiment_values.append(str(value_to_add))
-
-        self.table[sigma] = experiment_values
-        return True
-
-    def table_consistent(self) -> bool:
-        df = self.table
-        upper_table = df[df.index.get_level_values(0) == UPPER]
-
-        # Explore only those elements that are in the upper table
-        for actual_index, actual_row in upper_table.iterrows():
-            actual_state_name = actual_index[1]
-
-            for similar_index, similar_row in upper_table.iterrows():
-
-                if similar_index != actual_index and actual_row.equals(similar_row):
-                    similar_state = similar_index[1]
-
-                    for sigma in [s for s in self.alphabet if s not in self.table.columns.values]:
-                        successor_actual_state = self.concatenate_two_strings(actual_state_name, sigma)
-                        successor_similar_state = self.concatenate_two_strings(similar_state, sigma)
-
-                        successor_actual_row = df[df.index.get_level_values(1) == successor_actual_state].iloc[0]
-                        successor_similar_row = df[df.index.get_level_values(1) == successor_similar_state].iloc[0]
-
-                        if not successor_actual_row.equals(successor_similar_row):
-                            # Contradiction, because we say that the table is consistent,
-                            # but we found that a row is not
-                            # consistent so we add the column sigma to the table
-                            self.add_column(sigma)
-                            return False
-
-        return True
-
-    def fill_columns(self, states_to_add: list, is_upper_table: bool):
-        dict_to_add = {c: [] for c in self.table.columns}
-        states_to_add = [s for s in states_to_add if s not in self.table.index.get_level_values(1)]
-
-        for state_string in states_to_add:
-            for c in dict_to_add.keys():
-                string_to_ask = self.concatenate_two_strings(state_string, c)
-                state_belong_language = self.ask_if_string_belongs_language(string_to_ask)
-                dict_to_add[c].append(state_belong_language)
-
-        dict_to_add[TYPE_TABLE] = [UPPER if is_upper_table else LOWER] * len(states_to_add)
-        dict_to_add[STATE] = states_to_add
-        self.add_rows_to_table(dict_to_add)
-
-    def add_successors_row(self, current_state):
-        states_to_add = [self.concatenate_two_strings(current_state, sigma) for sigma in self.alphabet]
-
-        self.fill_columns(states_to_add, is_upper_table=False)
-
-    def table_close(self) -> bool:
-        df = self.table
-
-        upper_table = df[df.index.get_level_values(0) == UPPER]
-        lower_table = df[df.index.get_level_values(0) == LOWER]
-
-        for actual_index, actual_row in lower_table.iterrows():
-            # check if the row is in the upper table
-            if actual_row.values not in upper_table.values:
-                # We don't found the value in the upper row, then the table is not close
-
-                # Get the not closed state
-                current_state = actual_index[1]
-
-                # reset the index to update the value to the upper table
-                df.reset_index(inplace=True)
-                # Send the actual state (row) to the upper table
-                row = df.index[(df[TYPE_TABLE] == actual_index[0]) & (df[STATE] == actual_index[1])].values
-                type_table_values = df[TYPE_TABLE]
-                # We send the actual row to the upper table
-                type_table_values.loc[row] = UPPER
-                df[TYPE_TABLE] = type_table_values
-
-                # Set the index
-                df.set_index([TYPE_TABLE, STATE], inplace=True)
-
-                # Add his successors to the lower table
-                self.add_successors_row(current_state)
-                return False
-
-        return True
-
-    def get_dictionary_states(self):
-        df = self.table
-
-        df_upper = df[df.index.get_level_values(0) == UPPER]
-
-        name_states = {}
-        states_string = {}
-        i = 0
-        for actual_index, actual_value in df_upper.iterrows():
-            state_string = actual_index[1]
-
-            if state_string not in name_states:
-                name_states[state_string] = f"q{i}"
-                states_string[f"q{i}"] = str(state_string)
-                i += 1
-
-                for similar_index, similar_value in df.iterrows():
-                    similar_state = similar_index[1]
-                    if similar_state in name_states or not actual_value.equals(similar_value):
-                        continue
-
-                    name_states[similar_state] = name_states[state_string]
-
-        return name_states, states_string
 
     def get_dict_automaton(self):
         name_states, states_string = self.get_dictionary_states()
@@ -253,7 +102,7 @@ class LStartRegularLanguagesFromLog:
             state = transitions[state][c]
         return state in final_states
 
-    def deal_counterexample(self, string_counterexample):
+    def deal_counterexample_automatic(self, string_counterexample):
         final_string = []
 
         successors_to_add = []
@@ -296,59 +145,11 @@ class LStartRegularLanguagesFromLog:
 
             if not string_accepted:
                 # Counter example
-                self.deal_counterexample(s)
+                self.deal_counterexample_automatic(s)
                 return False
         return True
 
-    def save_automaton(self):
-        name_states, states_string = self.get_dictionary_states()
-        upper_table = self.table[self.table.index.get_level_values(0) == UPPER]
-        final_states = set(name_states[f] for f in upper_table[upper_table[EPSILON] == '1'].index.get_level_values(1)
-                           .values)
-
-        f = Digraph('finite_state_machine', filename='fsm.gv')
-        f.attr(rankdir='LR', size='8,5')
-        # Put initial state
-        f.attr('node', shape='plaintext')
-        f.node(' ')
-
-        # Put the final states
-        f.attr('node', shape='doublecircle')
-        for q_f in final_states:
-            f.node(q_f)
-
-        input_symbols = set(self.alphabet)
-
-        f.attr('node', shape='circle')
-        # Initial transition
-        f.edge(' ', name_states[EPSILON], label=' ')
-
-        # Transitions
-        for state_from, string_state_from in states_string.items():
-            for sigma in input_symbols:
-                string_state_to = self.concatenate_two_strings(string_state_from, sigma)
-                state_to = name_states[string_state_to]
-
-                f.edge(state_from, state_to, label=sigma)
-
-        f.view()
-
-    def run(self):
-        iteration = 0
-        while self.running:
-            self.table.to_csv(f"table_{iteration}.csv")
-            iteration += 1
-            if not self.table_consistent():
-                continue
-
-            if not self.table_close():
-                continue
-
-            if self.correct_automaton():
-                break
-        self.save_automaton()
-
 
 if __name__ == '__main__':
-    maestro = LStartRegularLanguagesFromLog("log_mod_3_characters.log")
+    maestro = LStartRegularLanguagesFromLog("log_pair_characters.example")
     maestro.run()
